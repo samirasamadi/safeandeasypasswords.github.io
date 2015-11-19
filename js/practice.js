b3e067ebca00e6919001f1ce3db3518e8f225cb6
Sanjay <ssood31@gatech.edu>
2015-11-19 10:11:17 -0500

    help now adds correct answer to textbox

--- js/practice.js
+++ js/practice.js
@@ -81,6 +81,8 @@ function boldKeyChar(i) {
   w3 += key.substring(i,i+1).bold();
   w3 += key.substring(i+1,key.length);
   document.getElementById("keyData").innerHTML = w3;
+  prevAns = document.getElementById("inputBox").value;
+  document.getElementById("inputBox").innerHTML = prevAns + key.substring(i,i+1);
 }
 function boldChallengeChar(i) {
   w2 = challenge.substring(0,i);
